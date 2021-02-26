74a7abb9f7f3fac7a7068c66eb26b80a2ea707dc
circlesharp <circlesharp@live.com>
2021-02-26 20:37:50 +0800

    2d 纹理 显示在 3d 场景上
    sub
    
    2d 纹理 显示在 3d 场景上

---
+++ wx-jump/src/game/controller.js
@@ -0,0 +1,32 @@
+import gameView from './view';
+import gameModel from './model';
+
+class GameController {
+  constructor() {
+    this.gameView = gameView;
+    this.gameModel = gameModel;
+  }
+
+  showGameOverPage = () => {
+    this.gameView.showGameOverPage();
+  };
+
+  restartGame = () => {
+    this.gameView.restartGame();
+  };
+
+  initPages() {
+    const gamePageCallbacks = {
+      showGameOverPage: this.showGameOverPage,
+    };
+
+    const gameOverPageCallbacks = {
+      gameRestart: this.restartGame,
+    };
+
+    this.gameView.initGamePage(gamePageCallbacks);
+    this.gameView.initGameOverPage(gameOverPageCallbacks);
+  }
+}
+
+export default new GameController();
---
+++ wx-jump/src/utils/event.js
@@ -0,0 +1,19 @@
+/* 维护一个 callback 数组，并且监听到事件后全部触发 */
+class Event {
+  constructor(sender) {
+    this._sender = sender;
+    this._listeners = [];
+  }
+
+  attach(callback) {
+    this._listeners.push(callback);
+  }
+
+  notify(args) {
+    this._listeners.forEach(listener => {
+      listener(sender, args);
+    });
+  }
+}
+
+export default Event;
